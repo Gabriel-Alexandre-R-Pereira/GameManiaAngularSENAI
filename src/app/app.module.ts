d3edb603a056af3e0d43b24144f3ae1b0f9b1f4b
Gabriel-Alexandre-R-Pereira <profissionalgabriel96@gmail.com>
2022-10-20 00:52:43 -0300

    Created LoginComponent and made the imports and route to src/app/app.module.ts and src/app/app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
