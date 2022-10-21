33f86331bc1b5c3586e1565b46ca8cfb1ca9cd2e
Gabriel-Alexandre-R-Pereira <profissionalgabriel96@gmail.com>
2022-10-20 23:51:46 -0300

    Imported User class, created its instance UserModel and passed the function ValidationLogin() from src/assets/JS/script.js to src/app/views/login/login.component.ts, then called it and UserModel's atibutes in src/app/views/login/login.component.html

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,43 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  UserModel = new User();
+  
+  // Validating Login
+
+  ValidationLogin() {
+
+    let Email = this.UserModel.email;
+    let Password = this.UserModel.password;
+
+    if ((Email === 'meuemail@exemplo.com') && (Password === 'minhasenha123')) {
+        const success = document.createElement('samp');
+
+        success.innerText = 'Login executado com sucesso!';
+
+        document.getElementById('PasswordContainer')!.appendChild(success);
+        success.setAttribute ('id', 'success');
+        document.getElementById('success')!.style.color = 'hsl(120, 100%, 35%)';
+    
+        setTimeout(() => {
+            success.style.display = 'none';
+        }, 1000);
+
+    } else {
+        const failure = document.createElement('samp');
+
+        failure.innerText = 'Senha ou e-mail incorretos!';
+
+        document.getElementById('PasswordContainer')!.appendChild(failure);
+        failure.setAttribute('id', 'failure');
+        document.getElementById('failure')!.style.color = 'hsl(0, 100%, 35%)';
+
+        setTimeout(() => {
+            failure.style.display = 'none';    
+        }, 1000);
+    }
   }
 
 }
